2993a4942401e24402446dc17332e65715c441ea
Japit <japit.kr@seshaasai.com>
2022-12-03 16:33:42 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideWarComponent } from './side-war/side-war.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideWarComponent,
+    NavBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/side-war/side-war.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-war',
+  templateUrl: './side-war.component.html',
+  styleUrls: ['./side-war.component.scss']
+})
+export class SideWarComponent {
+
+}
