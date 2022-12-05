6e63e734cdfb130c1e8e695fbbf3e42b5cc07092
Japit <japit.kr@seshaasai.com>
2022-12-05 13:17:38 +0530

    implementation of custom services

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BasicDataService } from '../customServices/basic-data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public percentageOf_CPU_Traffic:any
+  public numberOfLikes:any
+  public numberOfSales:any
+  public newMembers:any
+  constructor(
+    private homeData:BasicDataService
+  ){
 
+  }
+ ngOnInit(): void {
+   this.numberOfLikes= this.homeData.getHomeData().Likes
+   this.numberOfSales = this.homeData.getHomeData().Sales
+   this.newMembers = this.homeData.getHomeData().NewMembers
+   this.percentageOf_CPU_Traffic = this.homeData.getHomeData().CPU_Traffic
+ }
 }
--- src/app/side-war/side-war.component.ts
+++ src/app/side-war/side-war.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {BasicDataService} from "../customServices/basic-data.service"
 @Component({
   selector: 'app-side-war',
   templateUrl: './side-war.component.html',
   styleUrls: ['./side-war.component.scss']
 })
-export class SideWarComponent {
+export class SideWarComponent implements OnInit {
+  public userName:string = ""
+  public userImage:string=""
+  constructor(
+    private userData: BasicDataService,
+    ) { }
+
+  ngOnInit() {
+   this.userName = this.userData.getUsers().userName 
+   this.userImage = this.userData.getUsers().ImageUrl
+   
+  }
 
 }
