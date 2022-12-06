0ed5fc8d9d0487e2aaa3d1db65bb14c88f13276a
Japit <japit.kr@seshaasai.com>
2022-12-06 15:15:19 +0530

    added router links

--- src/app/customServices/basic-data.service.ts
+++ src/app/customServices/basic-data.service.ts
@@ -12,6 +12,6 @@ export class BasicDataService {
     return {CPU_Traffic:67, Likes:"56,788", Sales:"760", NewMembers: "2,000" }
   }
   notificationData(){
-    return [{ImageUrl:"http://localhost:4200/assets/dist/img/user1-128x128.jpg",name:"Brad Diesel"  ,message:"Call me whenever you can...", time: "4 hours ago" },{ImageUrl:"http://localhost:4200/assets/dist/img/user8-128x128.jpg", name:"John Pierce", message:"I got your message bro", time: "3 hours ago" },{ImageUrl:"http://localhost:4200/assets/dist/img/user3-128x128.jpg", name:"Nora Silvester", message:"The subject goes here", time: "2 hours ago" }]
+    return [{ImageUrl:"http://localhost:8858/assets/dist/img/user1-128x128.jpg",name:"Brad Diesel"  ,message:"Call me whenever you can...", time: "4 hours ago" },{ImageUrl:"http://localhost:8858/assets/dist/img/user8-128x128.jpg", name:"John Pierce", message:"I got your message bro", time: "3 hours ago" },{ImageUrl:"http://localhost:8858/assets/dist/img/user3-128x128.jpg", name:"Nora Silvester", message:"The subject goes here", time: "2 hours ago" }]
   } 
 }
